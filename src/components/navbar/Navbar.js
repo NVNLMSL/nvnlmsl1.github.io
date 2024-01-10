f3e42d62c5b32ae662ea2292eae44e480ab6c953
Navin Lamsal <navinlamsal58@gmail.com>
2024-01-10 21:09:11 +1100

    changes

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,21 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
-const Navbar = () => {
-    return(
-        <header class="sticky">
-            <nav>
-                <ul>
-                    <li><a href="home">Home</a></li>
-                    <li><a href="menu">Menu</a></li>
-                    <li><a href="about">About Us</a></li>
-                    <li><a href="contact">Contact Us</a></li>
+// const Navbar = () => {
+//     return(
+//         <header class="sticky">
+//             <nav>
+//                 <ul>
+//                     <li><a href="home">Home</a></li>
+//                     <li><a href="menu">Menu</a></li>
+//                     <li><a href="about">About Us</a></li>
+//                     <li><a href="contact">Contact Us</a></li>
             
-                </ul>
-            </nav>
-            <div class="animation"></div>
-        </header>
-    )
-}
+//                 </ul>
+//             </nav>
+//             <div class="animation"></div>
+//         </header>
+//     )
+// }
 
-export default Navbar;
+// export default Navbar;
+const Navbar = () => {
+    return (
+      <header className="sticky">
+        <nav>
+          <ul>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/menu">Menu</Link></li>
+            <li><Link to="/about">About Us</Link></li>
+            <li><Link to="/contact">Contact Us</Link></li>
+          </ul>
+        </nav>
+        <div className="animation"></div>
+      </header>
+    );
+  }
+  
+  export default Navbar;
